bbc38989f371fb1906532e468078fd1d7580a607
Mohak327 <mohak327@gmail.com>
2020-08-09 16:04:23 +0530

    .

--- css/js/script.js
+++ css/js/script.js
@@ -1,2 +1,19 @@
 // Animations init
 new WOW().init();
+// Carousel options
+
+$(".carousel").carousel({
+  interval: 3000,
+});
+
+$(document).ready(function () {
+  // Transition effect for navbar
+  $(window).scroll(function () {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 500) {
+      $(".navbar").addClass("solid");
+    } else {
+      $(".navbar").removeClass("solid");
+    }
+  });
+});
