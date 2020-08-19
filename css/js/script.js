b8eafddec96d8605cbcd3f0abc40fc73af4d8672
Mohak327 <mohak327@gmail.com>
2020-08-19 07:38:54 +0530

    view more button(fail)

--- css/js/script.js
+++ css/js/script.js
@@ -2,54 +2,30 @@
 new WOW().init();
 // Carousel options
 
-$('.modal-content').resizable({
-  //alsoResize: ".modal-dialog",
-  minHeight: 300,
-  minWidth: 300
+$(function () {
+  $('#login').click(function () {
+    $('#loginModal').modal('show');
+  });
+
+  $('#reserve').click(function () {
+    $('#reservation').modal('show');
+  });
+
+  $('#signup-btn').click(function () {
+    $('#loginModal').modal('hide');
+    $('#signup').modal('show');
+  });
 });
 
-
-$(".carousel").carousel({
-  interval: 3000,
+$('#carousel-example-1z').carousel({ interval: 2000 });
+$('#carouselButton').click(function () {
+  if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+    $('#carousel-example-1z').carousel('pause');
+    $('#carouselButton').children('span').removeClass('fa-pause');
+    $('#carouselButton').children('span').addClass('fa-play');
+  } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+    $('#carousel-example-1z').carousel('cycle');
+    $('#carouselButton').children('span').removeClass('fa-play');
+    $('#carouselButton').children('span').addClass('fa-pause');
+  }
 });
-
-
-<script type="text/javascript">
-      $(function () {
-        $("#login").click(function () {
-          $("#loginModal").modal("show");
-        });
-
-        $("#reserve").click(function () {
-          $("#reservation").modal("show");
-        });
-
-        $("#signup-btn").click(function () {
-          $("#loginModal").modal("hide");
-          $("#signup").modal("show");
-        });
-      });
-
-
-        {/* $("#nav-about").click(function (){
-          $("#nav-home").removeClass("active");
-          $("#nav-about").addClass("active");
-        }); */}
-
-      $(document).ready(function () {
-        $("#carousel-example-1z").carousel({ interval: 2000 });
-        $("#carouselButton").click(function () {
-          if ($("#carouselButton").children("span").hasClass("fa-pause")) {
-            $("#carousel-example-1z").carousel("pause");
-            $("#carouselButton").children("span").removeClass("fa-pause");
-            $("#carouselButton").children("span").addClass("fa-play");
-          } else if (
-            $("#carouselButton").children("span").hasClass("fa-play")
-          ) {
-            $("#carousel-example-1z").carousel("cycle");
-            $("#carouselButton").children("span").removeClass("fa-play");
-            $("#carouselButton").children("span").addClass("fa-pause");
-          }
-        });
-      });
-    </script>
